3a21873e693377ba8c85c5fe5e47b0de8cc2ce92
Kien T. Nguyen <v.kiennt54@vinai.io>
2022-11-04 16:44:57 +0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dotfiles
+
+### How to setup
+```
+git clone git@github.com:kiennt63/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+git submodule update --init --recursive
+```
--- setup.sh
+++ setup.sh
@@ -2,5 +2,19 @@
 
 mkdir -p ~/.local/bin
 
-ln -s $(realpath fetches/bunnyfetch) ~/.local/bin
-ln -s $(realpath .zshrc) ~/.local/bin
+# Make backup config
+cp -ra ~/.config ~/.config_old
+
+ln -sf $HOME/dotfiles/fetches/bunnyfetch ~/.local/bin
+ln -sf $HOME/dotfiles/zsh/.zshrc ~
+ln -sf $HOME/dotfiles/awesome ~/.config
+ln -sf $HOME/dotfiles/dunts ~/.config
+ln -sf $HOME/dotfiles/kitty ~/.config
+ln -sf $HOME/dotfiles/nvim ~/.config
+ln -sf $HOME/dotfiles/picom ~/.config
+ln -sf $HOME/dotfiles/ranger ~/.config
+ln -sf $HOME/dotfiles/rofi ~/.config
+ln -sf $HOME/dotfiles/starship.toml ~/.config
+
+echo "DONE SETUP DOTFILES"
+echo "PLEASE CHECK IF THINGS IF WORKING PROPERLY AND REMOVE THE OLD CONFIG"
