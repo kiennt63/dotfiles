674e39c1b3d0f0c75ebdd52d380853abe999d741
Kien T. Nguyen <neik@vai-kiennt54-l.local>
2022-11-22 22:36:44 +0700

    add kitty

--- set_theme.sh
+++ set_theme.sh
@@ -4,22 +4,22 @@ THEME=$1
 if [[ $THEME = "catppuccin" ]]; then
     sudo ln -sf "$HOME/dotfiles/nvim/lua/plugins/themes/catppuccin.lua" "$HOME/dotfiles/nvim/lua/plugins/themes/init.lua"
     sudo ln -sf "$HOME/dotfiles/awesome/theme/catppuccin.lua" "$HOME/dotfiles/awesome/theme/theme.lua"
-    sudo ln -sf "$HOME/dotfiles/kitty/frappe.conf" "$HOME/dotfiles/kitty/theme.conf"
+    sudo ln -sf "$HOME/dotfiles/kitty/themes/frappe.conf" "$HOME/dotfiles/kitty/themes/theme.conf"
     sudo ln -sf "$HOME/dotfiles/startpages/raichu_startpage/css/catppuccin.css" "$HOME/dotfiles/startpages/raichu_startpage/css/vars.css"
 elif [[ $THEME = "cat_mocha" ]]; then
     sudo ln -sf "$HOME/dotfiles/nvim/lua/plugins/themes/cat_mocha.lua" "$HOME/dotfiles/nvim/lua/plugins/themes/init.lua"
     sudo ln -sf "$HOME/dotfiles/awesome/theme/cat_mocha.lua" "$HOME/dotfiles/awesome/theme/theme.lua"
-    sudo ln -sf "$HOME/dotfiles/kitty/mocha.conf" "$HOME/dotfiles/kitty/theme.conf"
+    sudo ln -sf "$HOME/dotfiles/kitty/themes/mocha.conf" "$HOME/dotfiles/kitty/themes/theme.conf"
     sudo ln -sf "$HOME/dotfiles/startpages/raichu_startpage/css/mocha.css" "$HOME/dotfiles/startpages/raichu_startpage/css/vars.css"
 elif [[ $THEME = "nord" ]]; then
     sudo ln -sf "$HOME/dotfiles/nvim/lua/plugins/themes/nord.lua" "$HOME/dotfiles/nvim/lua/plugins/themes/init.lua"
     sudo ln -sf "$HOME/dotfiles/awesome/theme/nord.lua" "$HOME/dotfiles/awesome/theme/theme.lua"
-    sudo ln -sf "$HOME/dotfiles/kitty/nord.conf" "$HOME/dotfiles/kitty/theme.conf"
+    sudo ln -sf "$HOME/dotfiles/kitty/themes/nord.conf" "$HOME/dotfiles/kitty/themes/theme.conf"
     sudo ln -sf "$HOME/dotfiles/startpages/raichu_startpage/css/nord.css" "$HOME/dotfiles/startpages/raichu_startpage/css/vars.css"
 elif [[ $THEME = "gruvbox" ]]; then
     sudo ln -sf "$HOME/dotfiles/nvim/lua/plugins/themes/gruvbox.lua" "$HOME/dotfiles/nvim/lua/plugins/themes/init.lua"
     sudo ln -sf "$HOME/dotfiles/awesome/theme/gruvbox.lua" "$HOME/dotfiles/awesome/theme/theme.lua"
-    sudo ln -sf "$HOME/dotfiles/kitty/gruvbox-material-dark-medium.conf" "$HOME/dotfiles/kitty/theme.conf"
+    sudo ln -sf "$HOME/dotfiles/kitty/themes/gruvbox-material-dark-medium.conf" "$HOME/dotfiles/kitty/themes/theme.conf"
     sudo ln -sf "$HOME/dotfiles/startpages/raichu_startpage/css/nord.css" "$HOME/dotfiles/startpages/raichu_startpage/css/vars.css"
 else
     echo "Choosen theme not supported. Currently support"
