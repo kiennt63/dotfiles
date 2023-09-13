62702b7de3f4ec934883cb397d2c3ef55c943c75
Kien T. Nguyen <v.kiennt54@vinai.io>
2023-09-13 10:47:16 +0700

    update nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -69,38 +69,21 @@ require('lazy').setup({
         lazy = false,
         dependencies = {
             "nvim-tree/nvim-web-devicons",
-        },
+        }
+    },
+
+    -- session manager
+    {
+        'rmagatti/auto-session',
         opts = {
-            renderer = {
-                indent_markers = {
-                    enable = false,
-                    inline_arrows = false,
-                },
-                icons = {
-                    glyphs = {
-                        default = "",
-                        symlink = "",
-                        folder = {
-                            default = "",
-                            open = "",
-                            symlink = "",
-                            symlink_open = "",
-                        },
-                        git = {
-                            unstaged = "",
-                            staged = "✓",
-                            unmerged = "",
-                            renamed = "➜",
-                            untracked = "",
-                            deleted = "",
-                            ignored = "◌",
-                        },
-                    },
-                    show = {
-                        folder_arrow = false
-                    }
-                }
-            }
+            log_level = "error",
+            cwd_change_handling = {
+                restore_upcoming_session = true,    -- already the default, no need to specify like this, only here as an example
+                pre_cwd_changed_hook = nil,         -- already the default, no need to specify like this, only here as an example
+                post_cwd_changed_hook = function () -- example refreshing the lualine status line _after_ the cwd changes
+                    require("lualine").refresh()    -- refresh lualine so the new session name is displayed in the status bar
+                end,
+            },
         }
     },
 
@@ -303,6 +286,100 @@ require('lazy').setup({
         opts = {},
     },
 
+    -- Add tabs
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = { 'nvim-tree/nvim-web-devicons', 'moll/vim-bbye' },
+        -- opts = {
+        --     options = {
+        --         numbers = "none",                             -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+        --         close_command = "Bdelete! %d",                -- can be a string | function, see "Mouse actions"
+        --         middle_mouse_command = "Bdelete! %d",         -- can be a string | function, see "Mouse actions"
+        --         left_mouse_command = "buffer %d",             -- can be a string | function, see "Mouse actions"
+        --         right_mouse_command = ":BufferLineTogglePin", -- can be a string | function, see "Mouse actions"
+        --         -- NOTE: this plugin is designed with this icon in mind,
+        --         -- and so changing this is NOT recommended, this is intended
+        --         -- as an escape hatch for people who cannot bear it for whatever reason
+        --         indicator = {
+        --             icon = '▎', -- this should be omitted if indicator style is not 'icon'
+        --             style = 'icon',
+        --         },
+        --         buffer_close_icon = "",
+        --         -- buffer_close_icon = '',
+        --         modified_icon = "●",
+        --         close_icon = "",
+        --         -- close_icon = '',
+        --         left_trunc_marker = "",
+        --         right_trunc_marker = "",
+        --         --- name_formatter can be used to change the buffer's label in the bufferline.
+        --         --- Please note some names can/will break the
+        --         --- bufferline so use this at your discretion knowing that it has
+        --         --- some limitations that will *NOT* be fixed.
+        --         -- name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
+        --         --   -- remove extension from markdown files for example
+        --         --   if buf.name:match('%.md') then
+        --         --     return vim.fn.fnamemodify(buf.name, ':t:r')
+        --         --   end
+        --         -- end,
+        --         max_name_length = 30,
+        --         max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+        --         tab_size = 18,
+        --         diagnostics = false,    -- | "nvim_lsp" | "coc",
+        --         diagnostics_update_in_insert = false,
+        --         -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        --         --   return "("..count..")"
+        --         -- end,
+        --         -- NOTE: this will be called a lot so don't do any heavy processing here
+        --         -- custom_filter = function(buf_number)
+        --         --   -- filter out filetypes you don't want to see
+        --         --   if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+        --         --     return true
+        --         --   end
+        --         --   -- filter out by buffer name --   if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+        --         --     return true
+        --         --   end
+        --         --   -- filter out based on arbitrary rules
+        --         --   -- e.g. filter out vim wiki buffer from tabline in your work repo
+        --         --   if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+        --         --     return true
+        --         --   end
+        --         -- end,
+        --         offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+        --         show_buffer_icons = true,
+        --         show_buffer_close_icons = true,
+        --         show_close_icon = false,
+        --         show_tab_indicators = false,
+        --         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+        --         -- can also be a table containing 2 custom separators
+        --         -- [focused and unfocused]. eg: { '|', '|' }
+        --         separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+        --         enforce_regular_tabs = true,
+        --         always_show_bufferline = true,
+        --         sort_by = 'id', -- | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
+        --     },
+        --     highlights = {
+        --         fill = {
+        --             fg = '#2E3440',
+        --             bg = '#2E3440'
+        --         },
+        --         buffer_selected = {
+        --             bold = false,
+        --             italic = false,
+        --         },
+        --         separator_selected = {
+        --             fg = '#2E3440',
+        --         },
+        --         separator_visible = {
+        --             fg = '#2E3440',
+        --         },
+        --         separator = {
+        --             fg = '#2E3440',
+        --         },
+        --     };
+        -- }
+    },
+
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
 
@@ -338,3 +415,5 @@ require('plugins/config/treesitter')
 require('plugins/config/lsp')
 require('plugins/config/cmp')
 require('plugins/config/lualine')
+require('plugins/config/nvim-tree')
+require('plugins/config/bufferline')
