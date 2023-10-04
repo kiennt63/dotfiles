17e1c27f734cda3cf0314886336ce9920c223ce0
Kien T. Nguyen <v.kiennt54@vinai.io>
2023-10-04 13:33:13 +0700

    fix set_theme

--- set_theme.sh
+++ set_theme.sh
@@ -12,16 +12,16 @@ elif [[ $THEME = "cat_mocha" ]]; then
     sudo ln -sf "$HOME/dotfiles/kitty/themes/mocha.conf" "$HOME/dotfiles/kitty/themes/theme.conf"
     sudo ln -sf "$HOME/dotfiles/startpages/raichu_startpage/css/mocha.css" "$HOME/dotfiles/startpages/raichu_startpage/css/vars.css"
 elif [[ $THEME = "nord" ]]; then
-    sudo ln -sf "$HOME/dotfiles/nvim/lua/plugins/themes/nord.lua" "$HOME/dotfiles/nvim/lua/plugins/themes/init.lua"
+    sudo ln -sf "$HOME/dotfiles/nvim/lua/plugins/config/nord.lua" "$HOME/dotfiles/nvim/lua/plugins/config/theme.lua"
     sudo ln -sf "$HOME/dotfiles/awesome/theme/nord.lua" "$HOME/dotfiles/awesome/theme/theme.lua"
     sudo ln -sf "$HOME/dotfiles/kitty/themes/nord.conf" "$HOME/dotfiles/kitty/themes/theme.conf"
     sudo ln -sf "$HOME/dotfiles/startpages/raichu_startpage/css/nord.css" "$HOME/dotfiles/startpages/raichu_startpage/css/vars.css"
-    sudo ln -sf "$HOME/dotfiles/git/nord.gitconfig "$HOME/dotfiles/git/theme.gitconfig "
+    sudo ln -sf "$HOME/dotfiles/git/nord.gitconfig" "$HOME/dotfiles/git/theme.gitconfig"
 elif [[ $THEME = "gruvbox" ]]; then
-    sudo ln -sf "$HOME/dotfiles/nvim/lua/plugins/themes/gruvbox.lua" "$HOME/dotfiles/nvim/lua/plugins/themes/init.lua"
+    sudo ln -sf "$HOME/dotfiles/nvim/lua/plugins/config/gruvbox.lua" "$HOME/dotfiles/nvim/lua/plugins/config/theme.lua"
     sudo ln -sf "$HOME/dotfiles/awesome/theme/gruvbox-material.lua" "$HOME/dotfiles/awesome/theme/theme.lua"
     sudo ln -sf "$HOME/dotfiles/kitty/themes/gruvbox-material-dark-medium.conf" "$HOME/dotfiles/kitty/themes/theme.conf"
-    sudo ln -sf "$HOME/dotfiles/git/gruvbox.gitconfig "$HOME/dotfiles/git/theme.gitconfig "
+    sudo ln -sf "$HOME/dotfiles/git/gruvbox.gitconfig" "$HOME/dotfiles/git/theme.gitconfig"
     sudo ln -sf "$HOME/dotfiles/startpages/raichu_startpage/css/nord.css" "$HOME/dotfiles/startpages/raichu_startpage/css/vars.css"
 else
     echo "Choosen theme not supported. Currently support"
