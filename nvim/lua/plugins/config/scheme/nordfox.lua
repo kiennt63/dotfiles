94763429112c2ef97db8ba0605b2617757180106
Kien T. Nguyen <kien.nguyen@ovt.com>
2024-12-23 08:12:27 +0700

    remove neovim, change to submodule

--- nvim/lua/plugins/config/scheme/nordfox.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd.colorscheme 'nordfox'
-
-vim.api.nvim_set_hl(0, 'Cursorline', { link = 'ColorColumn' })
-vim.api.nvim_set_hl(0, 'NvimTreeWinSeparator', { link = 'TelescopeBorder' })
-vim.api.nvim_set_hl(0, 'WinSeparator', { link = 'TelescopeBorder' })
