7daa00af9b8e9456e42ca0c078d5da1b81956ad2
Kien T. Nguyen <v.kiennt54@vinai.io>
2025-01-04 19:40:43 +0800

    update sketchybar

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -4,6 +4,7 @@ sketchybar --add event aerospace_workspace_change
 
 for sid in $(aerospace list-workspaces --all); do
     sketchybar --add item space.$sid left \
+        --subscribe space.$sid space_windows_change \
         --subscribe space.$sid aerospace_workspace_change \
         --set space.$sid \
         padding_left=0 \
@@ -12,6 +13,8 @@ for sid in $(aerospace list-workspaces --all); do
         label.font="sketchybar-app-font:Regular:16.0" \
         label.padding_left=0 \
         label.padding_right=0 \
+        background.color=${PALETTE5} \
+        background.drawing=off \
         click_script="sketchybar --reload" \
         script="$CONFIG_DIR/plugins/aerospace.sh $sid"
 done
--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,54 +1,29 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
-
 source $HOME/dotfiles/sketchybar/plugins/palette.sh
 
-# echo $INFO >>~/test.log
+if [[ $SENDER = "space_windows_change" ]]; then
+    WIN_STATUS=$(aerospace list-windows --all --json --format "%{app-name} %{workspace}")
 
-icon_strip=""
-# while IFS= read -r app; do
-#     icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-# done < <(echo "$INFO" | grep "^$1 " | cut -d' ' -f2-)
+    has_app=0
+    while IFS= read -r app; do
+        has_app=1
+        icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app") "
+    done < <(echo "$WIN_STATUS" | jq -r ".[] | select(.workspace == \"$1\") | .[\"app-name\"]")
 
-# while IFS= read -r app; do
-#     icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-# done < <(echo "$INFO" | grep "^$1 " | cut -d' ' -f2-)
-has_app=0
-while IFS= read -r app; do
-    has_app=1
-    icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app") "
-done < <(echo "$INFO" | jq -r ".[] | select(.workspace == \"$1\") | .[\"app-name\"]")
-
-# workspace_info=$(echo "$INFO" | grep "^$1 " | cut -d' ' -f2-)
-# if [ -n "$workspace_info" ]; then
-#     local apps=($workspace_info)
-#     for app in "${apps[@]}"; do
-#         icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-#     done
-# fi
-
-if [[ $has_app = 1 ]]; then
-    if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-        sketchybar --set $NAME background.drawing=on \
-            background.color=${PALETTE5} \
+    if [[ $has_app = 1 ]]; then
+        sketchybar --set $NAME \
             label="$icon_strip" \
             label.padding_right=15
     else
-        sketchybar --set $NAME background.drawing=off \
-            label="$icon_strip" \
-            label.padding_right=15
+        sketchybar --set $NAME \
+            label.padding_right=0 \
+            label="$icon_strip"
     fi
-else
-    if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-        sketchybar --set $NAME background.drawing=on \
-            background.color=${PALETTE5} \
-            label.padding_right=0 \
-            label="$icon_strip"
+elif [[ $SENDER = "aerospace_workspace_change" ]]; then
+    if [[ "$1" = "$FOCUSED_WORKSPACE" ]]; then
+        sketchybar --set $NAME background.drawing=on
     else
-        sketchybar --set $NAME background.drawing=off \
-            label.padding_right=0 \
-            label="$icon_strip"
+        sketchybar --set $NAME background.drawing=off
     fi
 fi
