fbb829a5daf47f224f36e0ad4f7c40a5a753e6a3
Kien T. Nguyen <v.kiennt54@vinai.io>
2025-02-12 23:59:37 +0800

    add normal everforest fzf

--- zsh/fzf/everforest.sh
+++ zsh/fzf/everforest.sh
@@ -4,6 +4,6 @@
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
  --color=fg:#d3c6aa,bg:-1,hl:#7fbbb3
  --color=fg+:#d3c6aa,bg+:#374145,hl+:#7fbbb3
- --color=info:#a7c080,prompt:#d699b6,pointer:#ffffff
+ --color=info:#a7c080,prompt:#d699b6,pointer:#7fbbb3
  --color=marker:#a7c080,spinner:#d699b6,header:#d699b6
- --pointer='
+ --pointer=░'
